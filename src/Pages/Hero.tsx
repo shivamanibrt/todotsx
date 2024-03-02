74fdfbfac24c09d9d67f271672d5c63d64cc4af1
shivamanibrt <shivamanibrt03350@gmail.com>
2024-03-02 16:22:38 +1100

    installing tailwind

---
+++ src/Pages/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero :React.FC= () => {
+  return (
+    <div>
+      <div className="heading text-customPurple">Taskify</div>
+    </div>
+  )
+}
+
+export default Hero
